2eee0bc10cc1b9cbda91f555fd5522c68c097e2d
natalievmedrano <nataliemed0403@gmail.com>
2023-03-07 13:51:47 -0600

    created models for payments app

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    month = models.CharField(max_length=255)
+    year = models.IntegerField()
+    payment_type = models.CharField(max_length=255)
+    payment_due_date = models.DateField()
+    verify_payment = models.CharField(max_length=255)
+   
