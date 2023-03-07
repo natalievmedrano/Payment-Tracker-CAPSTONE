aa96d59aee79af1d0d899eef4e59862323fca348
natalievmedrano <nataliemed0403@gmail.com>
2023-03-07 14:56:00 -0600

    registered payments app in admin.py + updated views file

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,10 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PaymentSerializer
+from .models import Payment
 # Create your views here.
 
 
 @api_view(['GET'])
 def payments_list(request):
+    payments= Payment.objects.all()
 
+    serializer= PaymentSerializer(payments, many= True)
 
-    return Response('ok')
+    
+    return Response(serializer.data)
