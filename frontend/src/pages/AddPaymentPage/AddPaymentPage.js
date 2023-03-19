979e90841133b2216e87862c1c6406b606215305
natalievmedrano <nataliemed0403@gmail.com>
2023-03-19 13:17:06 -0500

    updated Bearer + token for post request

--- frontend/src/pages/AddPaymentPage/AddPaymentPage.js
+++ frontend/src/pages/AddPaymentPage/AddPaymentPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import axios from "axios";
-import './AddPaymentPage.css'
+import "./AddPaymentPage.css";
 
 import useAuth from "../../hooks/useAuth";
 import useCustomForm from "../../hooks/useCustomForm";
 import { useNavigate } from "react-router-dom";
 
-let intialPaymentInfo = {
+let initialValues = {
   payment_type: "",
   payment_amount: "",
   month: "",
@@ -18,7 +18,7 @@ const AddPaymentPage = () => {
   const [user, token] = useAuth();
   const navigate = useNavigate();
   const [formData, handleInputChange, handleSubmit] = useCustomForm(
-    intialPaymentInfo,
+    initialValues,
     addPayment
   );
 
@@ -29,8 +29,8 @@ const AddPaymentPage = () => {
         formData,
         {
           headers: {
-            Authorization: "Bearer" + token,
-          },
+            Authorization: "Bearer " + token,
+          }
         }
       );
       navigate("/");
@@ -41,42 +41,42 @@ const AddPaymentPage = () => {
   return (
     <div className="container">
       <form className="payementForm" onSubmit={handleSubmit}>
-          <input
-            placeholder="Enter Payment Name"
-            type="text"
-            name="payment_type"
-            value={formData.payment_type}
-            onChange={handleInputChange}
-          />
-          <input
-            placeholder="Enter Payment Amount"
-            type="text"
-            name="payment_amount"
-            value={formData.payment_amount}
-            onChange={handleInputChange}
-          />
-          <input
-            placeholder="Enter Payment Month"
-            type="text"
-            name="month"
-            value={formData.month}
-            onChange={handleInputChange}
-          />
-          <input
-            placeholder="Enter Payment Year"
-            type="text"
-            name="year"
-            value={formData.year}
-            onChange={handleInputChange}
-          />
+        <input
+          placeholder="Enter Payment Name"
+          type="text"
+          name="payment_type"
+          value={formData.payment_type}
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="Enter Payment Amount"
+          type="text"
+          name="payment_amount"
+          value={formData.payment_amount}
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="Enter Payment Month"
+          type="text"
+          name="month"
+          value={formData.month}
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="Enter Payment Year"
+          type="text"
+          name="year"
+          value={formData.year}
+          onChange={handleInputChange}
+        />
 
-          <input
-            placeholder="Enter Payment Due Date"
-            type="text"
-            name="payment_due_date"
-            value={formData.payment_due_date}
-            onChange={handleInputChange}
-          />
+        <input
+          placeholder="Enter Payment Due Date"
+          type="text"
+          name="payment_due_date"
+          value={formData.payment_due_date}
+          onChange={handleInputChange}
+        />
         <button type="submit">Add Payment</button>
       </form>
     </div>
