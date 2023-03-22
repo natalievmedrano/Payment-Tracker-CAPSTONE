dcd18a8e89dc028f0984c13e56ad7acf77e1c5c6
natalievmedrano <nataliemed0403@gmail.com>
2023-03-22 17:24:11 -0500

    hard coded months into sidebar + changed JS apps to JSX

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -5,7 +5,7 @@ import PaymentCard from '../PaymentCard/PaymentCard';
 
 
 const SideBar = ({payments}) => {
-    let filteredbyYear = payments.filter(payment => payment.year >= 2021)
+    let mapped
 
     //console.log(activePaymentId)
     return ( <section className='sidebar'>
@@ -13,11 +13,24 @@ const SideBar = ({payments}) => {
 
     <h2>PAYMENT HISTORY</h2>
 
+
     
-    <div onClick={filteredbyYear}>
+    <div >
         <li><a href="/2021">2021</a></li>
         <li><a href="/2022">2022</a></li>
         <li><a href="/2023">2023</a></li>
+        <li><a href="/january">January</a></li>
+        <li><a href="">February</a></li>
+        <li><a href="">March</a></li>
+        <li><a href="">April</a></li>
+        <li><a href="">May</a></li>
+        <li><a href="">June</a></li>
+        <li><a href="">July</a></li>
+        <li><a href="">August</a></li>
+        <li><a href="">September</a></li>
+        <li><a href="">October</a></li>
+        <li><a href="">November</a></li>
+        <li><a href="">December</a></li>
       
     
     </div>
